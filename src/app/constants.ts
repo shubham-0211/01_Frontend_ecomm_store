d8c04ca007dcdc717231ce9fa8cdf6def4be6c51
Shubham Ghongade <110917156+shubham-0211@users.noreply.github.com>
2025-01-23 02:50:52 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://a73ee1651e4d44159a387e9d4b8fc655-604485020.ap-south-1.elb.amazonaws.com/api/products',
+    BACKEND_API_URL: 'http://a73ee1651e4d44159a387e9d4b8fc655-604485020.ap-south-1.elb.amazonaws.com',
 };
